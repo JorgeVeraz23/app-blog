88d48bd95a9c46913773ca4f85e9fb83f8a1a762
JorgeVera <JorgeVeraZambrano@outlook.com>
2024-02-04 18:40:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+    <div>
+      <header>
+      <h1>Mi Blog Personal</h1>
+      <nav>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/acerca-de">Acerca de</NavLink>
+      </nav>
       </header>
+      <main>
+        <div>
+          <h2>Blog</h2>
+          <p>Esta es la pagina principal de nuestro sitio</p>
+        </div>
+        <div>
+          <h2>Pagina de Inicio</h2>
+          <ul>
+            <li>Articulo #1</li>
+            <li>Articulo #2</li>
+            <li>Articulo #3</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Acerca de</h2>
+          <p>Hola me llamo Carlos</p>
+        </div>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
