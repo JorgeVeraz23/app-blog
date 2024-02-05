64d3b94f325db2b389d515d640ae93fe4fadade2
JorgeVera <JorgeVeraZambrano@outlook.com>
2024-02-04 21:12:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Inicio from './componentes/Inicio';
 import Blog from './componentes/Blog';
 import AcercaDe from './componentes/AcercaDe';
+import Header from './componentes/Header';
+import styled from 'styled-components';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-      <h1>Mi Blog Personal</h1>
-      <nav>
-        <NavLink to="/">Inicio</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <NavLink to="/acerca-de">Acerca de</NavLink>
-      </nav>
-      </header>
-      <main>
+    <ContenedorPrincipal>
+      <Header />
+      <Main>
         <Routes>
         <Route path="/" element={
           <Inicio />
@@ -28,10 +23,23 @@ const App = () => {
           <AcercaDe />
         }/>
         </Routes>
-      </main>
-    </div>
+      </Main>
+    </ContenedorPrincipal>
     </BrowserRouter>
   );
 }
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129,129,129,0.1);
+`;
 
 export default App;
